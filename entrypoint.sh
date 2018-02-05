328899ced8c80945f54ae39f4923281f332c3783
kaergel <kaergel@b1-systems.de>
2018-02-05 20:59:27 +0100

    initial upload

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install $pythonpackages
+cp -v /lcd4linux.conf /etc/lcd4linux.conf
+chown -v root:root /etc/lcd4linux.conf
+chmod -v 0600 /etc/lcd4linux.conf
+cp -v /get_measuring_value.py /usr/lib/python2.7/site-packages/get_measuring_value.py
+chown -v root:root /usr/lib/python2.7/site-packages/get_measuring_value.py 
+exec "$@"
